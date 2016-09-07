3c4a528e9204f895fb29208e003608908ac01d80
Matt Thomson <red.cataclysm@gmail.com>
2016-09-07 11:28:00 -0400

    Basic Context Menu

--- contextMenu.js
+++ contextMenu.js
@@ -1,5 +1,26 @@
-function init() {
+/* onInstalled Handler */
+function onInstalledHandler() {
   console.log('Initializing...');
+  buildContextMenus();
 }
 
-chrome.runtime.onInstalled.addListener(init);
+function buildContextMenus() {
+  var props = {
+    id: 'heading',
+    title: 'Look ma!',
+    contexts: ['page'],
+    documentUrlPatterns: ['https://fiu.blackboard.com/*'],
+  };
+
+  chrome.contextMenus.create(props);
+}
+
+/* onClicked Handler */
+function onClickHandler(info, tab) {
+  console.log(info);
+  console.log(tab);
+}
+
+/* Attach Listeners */
+chrome.runtime.onInstalled.addListener(onInstalledHandler);
+chrome.contextMenus.onClicked.addListener(onClickHandler);
